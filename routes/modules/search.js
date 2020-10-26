1c0239b4c0419c5d56d27227c08a4997c8c70639
goater <goater1095@gmail.com>
2020-10-26 17:20:04 +0800

    feat: modify new page & search function

--- routes/modules/search.js
+++ routes/modules/search.js
@@ -1,25 +1,20 @@
-const express = require("express");
-const router = express.Router();
-const Rest = require("../../models/restaurant");
+const express = require('express')
+const router = express.Router()
+const Rest = require('../../models/restaurant')
 
 //set route
-router.get("/", (req, res) => {
-  const keyword = req.query.keyword.toLowerCase();
-  // 不會使用mongoose-find的includes的功能
+router.get('/', (req, res) => {
+  const keyword = req.query.keyword.toLowerCase()
   Rest.find()
     .lean()
     .then((rests) => {
-      const searchRes = rests.filter((rest) =>
-        rest.name.toLowerCase().includes(keyword)
-      );
-      const searchRes2 = rests.filter((rest) =>
-        rest.category.toLowerCase().includes(keyword)
-      );
-      const searchResult = searchRes.concat(searchRes2);
-      res.render("index", { resList: searchResult, keyword });
-      console.log(location.href);
+      const searchResult = rests.filter(
+        (rest) => rest.name.toLowerCase().includes(keyword) && rest.category.toLowerCase().includes(keyword)
+      )
+      res.render('index', { resList: searchResult, keyword })
+      console.log(location.href)
     })
-    .catch((error) => console.error(error));
-});
+    .catch((error) => console.error(error))
+})
 
-module.exports = router;
+module.exports = router
